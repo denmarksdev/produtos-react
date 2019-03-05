1ee7454ec162c42970ebcf1985ac47ada06543f8
Denis Marques <denmarksdev@gmail.com>
2019-03-05 19:07:01 -0300

    feat: Uso do json-server e carregamento das categorias deste servidor

--- app/src/components/Produtos.js
+++ app/src/components/Produtos.js
@@ -3,14 +3,41 @@ import { Route, Link } from 'react-router-dom'
 import ProdutosHome from './ProdutosHome'
 import Categoria from './Categoria';
 
+import axios from 'axios'
+
 export default class Produtos extends React.Component {
+
+    state = {
+        categorias: []
+    }
+
+    componentDidMount() {
+
+        axios.get('http://localhost:3001/categorias')
+            .then(res => res.data)
+            .then(categorias => {
+                this.setState({ categorias })
+            })
+    }
+
+    renderCategorias(cat,index) {
+        return (
+            <li key={index}>
+                <Link to={`/produtos/categoria/${cat.id}`}>Categoria {cat.id}</Link>
+            </li>
+        )
+    }
+
     render() {
         const { match } = this.props
+        const { categorias } = this.state
         return (
             <div className='row' >
                 <div className='col-md-2'>
                     <h3>Categorias</h3>
-                    <Link to='/produtos/categoria/1'>Categoria 1</Link>
+                    <ul>
+                        {categorias.map(this.renderCategorias)}
+                    </ul>
                 </div>
                 <div className='col-md-10' >
                     <h1>Produtos</h1>
